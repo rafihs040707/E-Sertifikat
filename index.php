198e692b6f870696a227e2ef4fce89294dd5097a
Rafi Haykal <devsoftware80@gmail.com>
2026-02-23 14:14:40 +0700

    Penambahan alert jika email atau password salah

--- index.php
+++ index.php
@@ -35,12 +35,40 @@ if (isset($_SESSION['login_error'])) {
     if (isset($_GET['logout']) && $_GET['logout'] == 'berhasil') {
         $durasi = 4000; // durasi dalam ms (4 detik)
     ?>
-        <div id="logoutAlert" class="alert alert-primary d-flex position-absolute w-100 align-items-center fade show" role="alert">
+        <div id="logoutAlert" class="alert alert-success d-flex position-absolute w-100 align-items-center fade show" role="alert">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
                 <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
             </svg>
             <div>
                 Anda telah berhasil keluar dari aplikasi E-Sertifikat
+            </div>
+        </div>
+
+        <script>
+            setTimeout(() => {
+                const alertEl = document.getElementById('logoutAlert');
+                if (alertEl) {
+                    const bsAlert = new bootstrap.Alert(alertEl);
+                    bsAlert.close();
+                }
+            }, <?= $durasi ?>);
+
+            window.history.replaceState({}, document.title, 'index.php');
+        </script>
+    <?php
+    }
+    ?>
+
+    <?php
+    if (isset($_GET['pesan']) && $_GET['pesan'] == 'gagal') {
+        $durasi = 4000; // durasi dalam ms (4 detik)
+    ?>
+        <div id="logoutAlert" class="alert alert-danger d-flex position-absolute w-100 align-items-center fade show" role="alert">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+            </svg>
+            <div>
+                Email atau password tidak valid. Silakan coba lagi.
             </div>
         </div>
 
