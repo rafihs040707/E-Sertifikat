98c7328ef70236079ac877073ed96b7bea81bfb8
Rafi Haykal <devsoftware80@gmail.com>
2026-02-25 11:08:24 +0700

    Pergantian icon

--- lo/dashboard.php
+++ lo/dashboard.php
@@ -14,13 +14,6 @@ $jumlah_sertifikat = mysqli_num_rows($data_sertifikat);
     <title>Dashboard Liaison Officer</title>
 </head>
 
-<div class="container mt-3">
-    <div class="jumbotron">
-        <h1 class="text-center">Selamat, Anda Berhasil Login.! <br> Sebagai <?php $p = $_SESSION['role'];
-                                                                    echo $p; ?></h1>
-    </div>
-</div>
-
 <div class="container">
     <div class="row">
         <div class="col-lg-12">
@@ -28,7 +21,7 @@ $jumlah_sertifikat = mysqli_num_rows($data_sertifikat);
                 <div class="col-lg-3 mt-4">
                     <div class="card">
                         <div class="card-body text-center" style="background-color: #F5CF24;">
-                            <img src="<?= BASE_URL ?>image/iconmedali.png" class="ukuran my-3">
+                            <img src="<?= BASE_URL ?>image/certificate.png" class="ukuran my-3">
                             <h3 class="my-2"><?php echo $jumlah_sertifikat ?></h3>
                             <h5 class="my-3">Sertifikat</h5>
                         </div>
